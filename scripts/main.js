7d5474eb16aba44c110e2ab3461378ef95187058
johnpascual <ashleypascual@hotmail.es>
2022-07-13 17:47:48 +0100

    made card

--- scripts/main.js
+++ scripts/main.js
@@ -18,12 +18,38 @@ function removeForm (e) {
     form.style = "display: none"
     // Selecting the card container
     const container = document.getElementById("cardContainer")
-    // Create a card element
-    const newCard = document.createElement("div")
-    // add some text to the div
-    newCard.textContent = form.greet.value
+    // Create a div element
+    const newDiv = document.createElement("div")
+    // Add a card class to the div
+    newDiv.classList.add("card")
     // add element to the page
-    container.appendChild(newCard)
+    container.appendChild(newDiv)
+    // Create a img element
+    const newImg = document.createElement("img")
+    // Add a src class to the image
+    newImg.classList.add("src")
+    // Add a link to the source of the image
+    newImg.src = "https://www.w3schools.com/howto/img_avatar.png"
+    // add element to the page
+    newDiv.appendChild(newImg)
+    // Create a div element
+    const newDiv2 = document.createElement("div")
+    // Add a card class to the div
+    newDiv2.classList.add("container")
+    // add element to the page
+    newDiv.appendChild(newDiv2)
+    // Create a h4 element
+    const newhFour = document.createElement("h4")
+    // Add greeting to the h4
+    newhFour.textContent = form.greet.value
+    // add element to the page
+    newDiv2.appendChild(newhFour)
+    // Create a p element
+    const newP = document.createElement("p")
+    // Add message to the p
+    newP.textContent = form.message.value
+    // add element to the page
+    newDiv2.appendChild(newP)
 }
   
 document.getElementById("greeting-form").addEventListener("submit", removeForm)
